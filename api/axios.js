236147e769e1674a00b39a9faa4dc73bfcfb2a4b
nareksvazyan <65123969+nareksvazyan@users.noreply.github.com>
2026-02-12 21:09:32 +0400

    Update axios baseURL to include '/api'

--- api/axios.js
+++ api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-  baseURL: "https://ggrc-backend-hr0d.onrender.com" , //55555
+  baseURL: "https://ggrc-backend-hr0d.onrender.com/api" , //55555
   headers: {
     "Content-Type": "application/json",
   },
